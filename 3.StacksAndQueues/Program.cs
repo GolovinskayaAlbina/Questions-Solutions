186917079743abcea6e401457bd3330b34caccf9
DESKTOP-FDHI8C2\a_gol <a_golovinskaya@mail.ru>
2018-07-26 13:13:22 +0300

    set of stacks

---
+++ 3.StacksAndQueues/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stack = new SetOfStacks<int>(3);
+            for (int i=0;i<10;i++){
+                stack.Push(i);
+            }
+             stack.PopAt(1);
+             stack.PopAt(1);
+             stack.PopAt(1);
+            for (int i=0;i<7;i++){
+                Console.WriteLine(stack.Pop());
+            }
+        }
+    }
+}
